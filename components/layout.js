d4b8538b6b34a63fb7d0392b4ca8b2a0712bc426
goro-ishikura <rouland.do@gmail.com>
2023-04-17 00:09:40 +0900

    Add link

--- components/layout.js
+++ components/layout.js
@@ -13,28 +13,18 @@ const Layout = ({ children, home }) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        {home ? (
-          <>
-            <img
-              src="/images/logo_square.png"
-              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        ) : (
-          <>
-            <img
-              src="/images/logo_square.png"
-              className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
-          </>
-        )}
+        <Link href="https://neutcode.com/">
+          <img
+            src="/images/logo_square.png"
+            className={`${utilStyles.borderCircle} ${styles.headerHomeImage}`}
+          />
+        </Link>
+        <h1 className={utilStyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
       {!home && (
         <div>
-          <Link href="/">← ホームへ戻る</Link>
+          <Link href="https://neutcode.com/">← ホームへ戻る</Link>
         </div>
       )}
     </div>
