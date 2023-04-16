7a36930d5a5928917cc43a13cdd288f1be1dbbaf
goro-ishikura <rouland.do@gmail.com>
2023-04-16 16:35:32 +0900

    initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,41 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+import { getAllPostsIds, getPostData } from "../../lib/post";
+import utilStyles from "../../styles/utils.module.css";
+
+export const getStaticPaths = async () => {
+  const paths = getAllPostsIds();
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingX1}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>{postData.date}</div>
+        <div
+          dangerouslySetInnerHTML={{ __html: postData.blobContentHTML }}
+        ></div>
+      </article>
+    </Layout>
+  );
+};
+
+export default Post;
